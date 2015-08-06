f44bc73ea19a5bab6fa501de0e5e6f383af5774c
Rene Loperena <rene@implugd.com>
2015-08-05 18:50:29 -0700

    Adds basic implementation to serve static files on server. Adds Morgan.

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+var http = require('http');
+
+var Application = function() {
+
+    var self = this;
+
+    self.setupVariables = function() {
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+        if (typeof self.ipaddress === "undefined") {
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 0.0.0.0');
+            self.ipaddress = "0.0.0.0";
+        };
+    };
+
+
+    self.initializeServer = function() {
+        self.app = express();
+        self.app.use(morgan('combined'));
+        self.app.use(express.static('public'));
+        
+    };
+
+
+    self.initialize = function() {
+        self.setupVariables();
+        self.initializeServer();
+    };
+
+    self.start = function() {
+        self.app.listen(self.port, self.ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+        });
+    };
+
+};  
+
+var app = new Application();
+app.initialize();
+app.start();
